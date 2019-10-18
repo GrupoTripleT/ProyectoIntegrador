71ecacc371ad327c810f59b039f018eaafea6917
atupilojon <atupilojon@gmail.com>
2019-10-18 18:22:01 -0300

    Clase

---
+++ srcTest/packageProyectoIntegrador/BusquedaTest.java
@@ -0,0 +1,22 @@
+package packageProyectoIntegrador;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class BusquedaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
