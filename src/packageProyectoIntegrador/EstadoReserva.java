8cd41e9605523024c7045df7c783dcfe9b4a2391
Pablo <trochepablodaniel@gmail.com>
2019-10-19 22:18:12 -0300

    add fix files

--- src/packageProyectoIntegrador/EstadoReserva.java
+++ src/packageProyectoIntegrador/EstadoReserva.java
@@ -2,20 +2,8 @@ package packageProyectoIntegrador;
 
 public abstract class EstadoReserva {
 
-	public EstadoReserva() {
-		setEmailManager(new ServicioSMTP());
-	}
-
-	private IEmailManager emailManager;
+	public IEmailManager emailManager;
 
 	abstract void procesarReserva(Reserva r);
-
-	protected IEmailManager getEmailManager() {
-		return emailManager;
-	}
-
-	protected void setEmailManager(IEmailManager emailManager) {
-		this.emailManager = emailManager;
-	}
 
 }
